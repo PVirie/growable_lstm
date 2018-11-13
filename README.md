117360ad3bc4a0b5ba3e000af82861258ce536fa
tao <tao>
2018-11-13 17:05:20 +0700

    fixed test, adjusting hyper params.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # growable_lstm
 A collection of weak LSTMs for gradient boosting.
+
+## Introduction
+There are times when we have to deal with variable length data. And for most of those times, LSTM is the best available choice. The ability to deal with variable length data, the capacity to maintain information for an indefinite time, and the flexibility to stack more of them to form a more complex model are the core advantage that make LSTM one of the most popular algorithms even after decades of its first revelation. But it is not alone in the spotlights. Gradient boost is another algorithm that always works out of the box and has been extensively used in data competition around the world.
+
+Here comes the thesis: can I merge the benefits of these two algorithms together into a single model? I hereby present Growable LSTM as a model that has the potential of both worlds.
+
+## Prerequisites
+
+* Python 3
+* TensorFlow
+* numpy
+
+## License
+
+MIT free of use. 
--- main.py
+++ main.py
@@ -6,46 +6,62 @@ import os
 
 root = os.path.dirname(os.path.abspath(__file__))
 
-fashion_mnist = tf.keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+mnist = tf.keras.datasets.fashion_mnist
+# mnist.load_data(os.path.join(root, "artifacts", "fashion_mnist.npz"))
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 
 x_train = train_images.reshape(-1, 28, 28).astype(np.float32) / 255.0
 x_test = test_images.reshape(-1, 28, 28).astype(np.float32) / 255.0
+y_train = train_labels
+y_test = test_labels
+print(x_train.shape, x_test.shape)
 
 
 def init():
     rnn = Cells.Aggregated_Cell([28, 10])
-    booster = Boost.Classification_Gradient_Boost(rnn, 10, init_rate=0.01, rate_multiplier=1.0, gradient_learning_rate=0.001, lambda_learning_rate=0.001)
+    booster = Boost.Classification_Gradient_Boost(rnn, 10, init_rate=0.01, rate_multiplier=1.0, gradient_learning_rate=0.0001, lambda_learning_rate=0.001)
 
-    dataset = tf.data.Dataset.from_tensor_slices((x_train, train_labels)).shuffle(1024 * 16).batch(100)
-    testset = tf.data.Dataset.from_tensor_slices((x_test, test_labels)).batch(100)
+    # Smith, S. L., Kindermans, P. J., Ying, C., & Le, Q. V. (2017). Don't decay the learning rate, increase the batch size. arXiv preprint arXiv:1711.00489.
+    dataset_prebatch = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(1024 * 16)
+    dataset_batches = []
+    dataset_batches.append(dataset_prebatch.batch(50))
+    dataset_batches.append(dataset_prebatch.batch(100))
+    dataset_batches.append(dataset_prebatch.batch(150))
+    dataset_batches.append(dataset_prebatch.batch(200))
 
-    iter = tf.data.Iterator.from_structure(dataset.output_types, dataset.output_shapes)
-    train_init_op = iter.make_initializer(dataset)
+    testset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(100)
+
+    iter = tf.data.Iterator.from_structure(testset.output_types, testset.output_shapes)
+    train_init_ops = []
+    for item in dataset_batches:
+        train_init_ops.append(iter.make_initializer(item))
     test_init_op = iter.make_initializer(testset)
 
     inputs, targets = iter.get_next()
 
     results, confidence, training_ops = booster.build_graph(inputs, targets)
-    accuracy = tf.metrics.accuracy(targets, results)
+    accuracy = tf.reduce_sum(tf.cast(tf.equal(tf.cast(targets, tf.int64), results), tf.float32)) / 100
 
+    sess.run(tf.local_variables_initializer())
     sess.run(tf.global_variables_initializer())
 
     saver = tf.train.Saver()
 
-    return accuracy, training_ops, train_init_op, test_init_op, saver
+    return accuracy, training_ops, train_init_ops, test_init_op, saver
 
 
 def train(model, sess=None):
     print("train")
 
     training_ops = model[1]
-    train_init_op = model[2]
+    train_init_ops = model[2]
     saver = model[4]
 
-    for i in range(len(training_ops)):
-        for j in range(10):
+    total_boost = len(training_ops)
+    for i in range(total_boost):
+        train_init_op = train_init_ops[int(i * len(train_init_ops) / total_boost)]
+        for j in range(100):
             sess.run(train_init_op)
             while True:
                 try:
@@ -53,7 +69,7 @@ def train(model, sess=None):
                 except tf.errors.OutOfRangeError:
                     print(i, j, c)
                     break
-        for j in range(10):
+        for j in range(100):
             sess.run(train_init_op)
             while True:
                 try:
@@ -64,7 +80,7 @@ def train(model, sess=None):
         saver.save(sess, os.path.join(root, "weight_sets", "model.ckpt"))
 
 
-def test(data, model, sess=None):
+def test(model, sess=None):
     print("test")
 
     accuracy_op = model[0]
@@ -74,9 +90,9 @@ def test(data, model, sess=None):
     saver.restore(sess, os.path.join(root, "weight_sets", "model.ckpt"))
 
     sess.run(test_init_op)
+    accuracy = 0
+    count = 0
     while True:
-        accuracy = 0
-        count = 0
         try:
             acc = sess.run(accuracy_op)
             accuracy = accuracy + acc
